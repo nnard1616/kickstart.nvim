5f97b593777436eb8b45f881eb4368c3aab9e8f1
Nathan Nard <nnard1616@gmail.com>
2023-05-06 11:54:30 -0500

    activated gopls lsp

--- init.lua
+++ init.lua
@@ -410,7 +410,7 @@ end
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
