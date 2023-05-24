1041a99f1672c7bddb29ab9cda21b310a99ebfa5
Nathan Nard <nnard1616@gmail.com>
2023-05-23 19:24:13 -0500

    latex

--- init.lua
+++ init.lua
@@ -290,7 +290,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'html', 'css', 'yaml', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'make','cmake'},
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'html', 'css', 'yaml', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'make','cmake', 'latex'},
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
